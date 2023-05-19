6a003037bc887e2fcd42e530df2521036253f31f
GenericProgram <gddiamondnoob@gmail.com>
2023-05-19 12:02:08 -0700

    update

---
+++ src/game/items/Item.java
@@ -0,0 +1,32 @@
+package game.items;
+
+public class Item {
+    String name;
+    int clicksPerSecond=0;
+    int cost=0;
+    int amountOwned=0;
+
+    public int getClicksPerSecond() {
+        return clicksPerSecond;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmountOwned() {
+        return amountOwned;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setClicksPerSecond(int clicksPerSecond) {
+        this.clicksPerSecond = clicksPerSecond;
+    }
+}
